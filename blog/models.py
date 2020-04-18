35712f057fdf1a5aaf17f79a9f5607d45c837f60
vivek <vivekanandan.k@nutanix.com>
2020-04-18 22:16:58 +0530

    Initial page layout

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Create a blog models
+
+# Add the blog app to the settings
+
+# Create a migration
+
+# Migrate
+
+# Add to the admin
+
+class Blog(models.Model):
+    title = models.CharField(max_length= 200)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
